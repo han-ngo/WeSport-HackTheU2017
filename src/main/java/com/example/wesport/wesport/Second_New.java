c7542d2f0e7b7ae20357cbee8d5e7a2641cbbcac
han-ngo <gh.hanngo@gmail.com>
2016-11-12 11:42:14 -0700

    temp

--- src/main/java/com/example/wesport/wesport/Second_New.java
+++ src/main/java/com/example/wesport/wesport/Second_New.java
@@ -6,8 +6,20 @@ import android.os.Bundle;
 public class Second_New extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_second__new);
+        setContentView(R.layout.main);
+
+        GridView gridview = (GridView) findViewById(R.id.gridview);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(HelloGridView.this, "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
 }
