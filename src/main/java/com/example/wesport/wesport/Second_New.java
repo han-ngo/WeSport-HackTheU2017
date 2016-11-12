3035ab27adb8149f7dbddef84270a59399cfc760
han-ngo <gh.hanngo@gmail.com>
2016-11-12 10:49:11 -0700

    2 Screens
    1st Screen
    2nd Screen (New Match)

---
+++ src/main/java/com/example/wesport/wesport/Second_New.java
@@ -0,0 +1,13 @@
+package com.example.wesport.wesport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Second_New extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second__new);
+    }
+}
